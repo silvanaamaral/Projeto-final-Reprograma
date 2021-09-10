5ffddba202833a748a30ab9fb4e376cbbd1509f7
silvanaamaralpe@gmail.com <amaral>
2021-09-10 00:25:09 -0300

    alterações no arquivo app.js

--- aloCamelo/src/app.js
+++ aloCamelo/src/app.js
@@ -5,16 +5,7 @@ const index = require("./routes/index");
 const adminRouter = require('./routes/adminRoutes');
 const locais = require('./routes/locaisRoutes')
 
- /* app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Request-With, Content-Type, Accept"
-  );
-  next();
-});  */
-
-const database = require('../src/data/database')
+ const database = require('../src/data/database')
 database.connect()
 
 app.use(express.json())
